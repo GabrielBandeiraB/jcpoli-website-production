6c1a9170696df31e0267a5b610e7902eeacfeebe
gabriel bandeira <gabrielvillarealbandeira@gmail.com>
2023-04-24 07:56:47 -0300

    correção de erro merge

--- src/models/speakers.ts
+++ src/models/speakers.ts
@@ -1,6 +1,5 @@
 const speakers_jcpoli = [
   {
-<<<<<<< HEAD
     name: 'Lorem Ipsum',
     institution: 'Lorem Ipsum',
     speaking: 'Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum',
@@ -14,26 +13,6 @@ const speakers_jcpoli = [
       place: 'Online: Youtube'
     }
   },
-=======
-    name: "Michael Machado",
-    institution: "MRV Engenharia",
-    speaking: "Processo construtivo da parede de concreto e produtividade",
-    description: `Engenheiro Civil, formado na Uni-Goiás em 2019/2, atuando a 7 anos na construção civil e a 2 anos como Engenheiro residente de obras em parede de concreto pela MRV ENGENHARIA. Pós-Graduando em Gestão de Projetos e Processos pelo IPOG. 
-`,
-    subjectResume: `
-    Processo construtivo da parede de concreto e produtividade 
-`,
-    img: "/assets/img/speakers/Lucas Ribeiro.png",
-    customSize: "100%",
-    datentime: {
-      date: "09/05",
-      time: "09:00-10:30",
-      place: "Presencial",
-    },
-  },
-
-  
->>>>>>> 5c5486fbf727b398e39658b3fe616d78b800a91c
 ];
 
 export default speakers_jcpoli;
