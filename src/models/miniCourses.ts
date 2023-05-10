8c81c6ed238d3ff40b1cd34a7be81112abb2a758
gabriel bandeira <gabrielvillarealbandeira@gmail.com>
2023-05-10 13:57:48 -0300

    inserção link

--- src/models/miniCourses.ts
+++ src/models/miniCourses.ts
@@ -42,7 +42,8 @@ let Courses = [
     desc: `Como usar o Git/Github? Minicurso presencial, porém com gravação pelo Teams.`,
     date: '10/05',
     time: '17:00-19:00',
-    place: 'Presencial: Laboratório 101, bloco C, área II'
+    place: 'Presencial: Laboratório 101, bloco C, área II e Transmissão pelo Teams',
+    link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_Zjk4YTIwZGYtYjRhMC00NGQ5LTk4ZGQtODhiYmVkNGRlZGJk%40thread.v2/0?context=%7b%22Tid%22%3a%2273319f42-8908-4b89-9f8d-558cf4d5d776%22%2c%22Oid%22%3a%224d5d6768-2ee9-4adc-ac86-4da09deed785%22%7d"
   
   },
   
