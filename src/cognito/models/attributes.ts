4ea5a8cdc9ac43820d574e75a8d0b867d4d05bf1
valteci <valtecijunior@gmail.com>
2023-10-10 23:13:30 -0300

    remoção de arquivos desnecessários

--- src/cognito/models/attributes.ts
+++
@@ -1,8 +0,0 @@
-export default interface AttributeList {
-    name: string;
-    family_name: string;
-    email: string;
-    birthdate: string;
-    phone_number: string;
-    "custom:type": string;
-}
