2392676085eaf86d9646837deaab20678c7415c0
Paulo Henrique <55743232+PauloH290199@users.noreply.github.com>
2024-03-24 17:37:28 -0300

    Update exposicao.ts
    Atualização das exposições.

--- src/storage/programacao/exposicao.ts
+++ src/storage/programacao/exposicao.ts
@@ -1,41 +1,26 @@
 const Exposicao_Design = [
   {
-    title: "Povos originários — os caminhos da memória",
-    instructor: "Marcos Costa de Freitas",
-    institution: "",
-    desc: `Os povos originários são guardiões de tradições de verdadeiros tesouros imateriais, infelizmente esta é uma ideia recente. No passado muito se perdeu por falta consciência do valor e de registros adequados. Em busca do resgate da memória perdida iniciei um trabalho autoral para reconstruir imagens de alguns desses povos.
-        Trabalhando com descrições textuais, gravuras antigas do período colonial e estudo comparado de povos vizinhos, venho fazendo um percurso cujo objetivo é a reconstituição da memória perdida. Mas é um trabalho ainda em fase inicial, os desenhos e as abordagens de reconstituição representam apenas ensaios, o trabalho encontra-se na fase processual. 
-        A maioria dos desenhos da exposição são de povos de culturas vivas, essa abordagem é um estudo de situações similares, muito útil para tratar os registros dos povos antigos. Dessa forma, a exposição é um ensaio de um estudo exploratório de um tema muito extenso no tempo e na geografia. 
-    `,
-    date: "09/05 até 11/05",
+    title: "xx",
+    instructor: "xx",
+    institution: "xx",
+    desc: `xx`,
+    date: "xx",
     time: "",
-    place: "PROA, área II",
-    image: "assets/img/slider/expo_design.jpeg"
+    place: "xx",
+    image: "xx"
   },
 ]
 
 const Exposicao_Arquitetura = [
   {
-    title: "TFC -1 | Trabalho de Fim de Ciclo 1",
+    title: "xx",
     instructor: "",
-    institution: "PUC Goiás",
-    desc: `É uma atividade de encerramento do Primeiro Ciclo de Aprendizagem, que se inicia no 1º período e finaliza no 3º período, para o turno matutino, e 4º período, para o turno noturno. Consiste em um projeto integrado que contempla a aplicação dos conteúdos das disciplinas que compõem esse ciclo.  
-
-      O trabalho tem por objetivo evidenciar os conhecimentos, habilidades e atitudes incorporadas pelo aluno durante o 1º ciclo, que são essenciais para sua passagem para o 2º ciclo.  Quais sejam:     
-      
-      Demonstrar criatividade e sensibilidade projetual e artística; 
-      
-      Evidenciar conhecimentos, habilidades e competências adquiridos nas disciplinas  
-      
-      que compõem o 1º ciclo;   
-      
-      Demonstrar capacidade de expressão oral e gráfica, por meio de desenhos e maquetes; 
-      
-      Desenvolver exercício integrado das disciplinas do 1º ciclo com experimentações de metodologia de projeto. `,
-    date: "09/05 até 16/05",
-    time: "Abertura da Exposição dia 09/05 às 18:00",
-    place: "Foyer dos auditórios 2 e 3 na área II",
-    image: "assets/img/slider/expo_arqui.jpg"
+    institution: "xx",
+    desc: `xx`,
+    date: "xx",
+    time: "xx",
+    place: "xx",
+    image: "xx"
   },
 ]
 
